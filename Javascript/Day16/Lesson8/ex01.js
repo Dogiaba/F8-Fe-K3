e875a359efdd02908c778e9a7de54e74fcc6980b
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-08-12 00:40:22 +0700

    update day16

--- Javascript/Day16/Lesson8/ex01.js
+++ Javascript/Day16/Lesson8/ex01.js
@@ -1,15 +1,24 @@
 // S = 1 + 1/2 + 1/3 + 1/4 + 1/5 +…+1/N
 
-var n = prompt("Enter n: ");
-var s = 0;
-var i = 1 ;
-if (n < 1) {
-  console.log("Enter number >= 1");
-} else {
-    // // s = n*(1.0+n)/2;
-    // // s= s+(i*1.0)/(i+1.0);
-    // s += i/i;
-    // i++;
+
+
+var n = prompt("Nhập vào số n : ");
+n = +n;
+
+function S(n) {
+    if (n < 0){
+        return "Enter number > 0";
+    } 
+    else if (n === 0) {
+        return 0;
+    }
+    else if  (n === 1){
+        return 1;
+    } 
+    else {
+        return S(n - 1) + (1 / n); 
+    }
 }
 
-console.log(s);
+console.log(S(n));
+document.write(`Kết quả là : ${S(n)}`);
