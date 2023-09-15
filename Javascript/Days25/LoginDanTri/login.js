578df3e4f10bb2dfbd5669dddce220cb92b0ba77
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-09-15 23:33:33 +0700

    upday25

---
+++ Javascript/Days25/LoginDanTri/login.js
@@ -0,0 +1,15 @@
+let tab = document.querySelector(".form");
+let tabHeader = document.querySelector(".tab_header");
+let tabHeadElement = tab.querySelectorAll(".tab_header > div");
+let tabBody = tab.querySelector(".tab_body");
+let tabBodyElement = tab.querySelectorAll(".tab_body > div");
+
+
+for(let i = 0; i<tabHeadElement.length; i++){
+    tabHeadElement[i].addEventListener("click",function(){
+        tabHeader.querySelector(".active").classList.remove("active");
+        tabHeadElement[i].classList.add("active");
+        tabBody.querySelector(".active").classList.remove("active");
+        tabBodyElement[i].classList.add("active");
+    })
+}
