5bcc3f7fa7c5c5fa712111438d3d86c29241112d
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-10-13 07:05:29 +0700

    day33

---
+++ Javascript/Day33/downloadLink.js
@@ -0,0 +1,53 @@
+var counter = document.querySelector(".counter");
+var btnLink = document.querySelector(".btn-link");
+
+var counterNumber = 20;
+let checkTime;
+let focusOut = false;
+
+
+
+function startDown(currentTime) {
+  if (!checkTime) checkTime = currentTime;
+  if (currentTime - checkTime > 1000) {
+    counterNumber--;
+    checkTime = currentTime;
+    counter.innerText = counterNumber;
+  }
+  if (counterNumber > 0) {
+    requestAnimationFrame(startDown);
+  } else {
+    btnLink.disabled = false;
+  }
+}
+requestAnimationFrame(startDown);
+
+btnLink.addEventListener("click", function(){
+    window.location.href="https://anonystick.com/blog-developer/performance-javascript-3-vi-du-nay-co-the-chung-minh-2020121088424197"
+})
+
+// Sử dụng event blur or focus phát hiện user chuyển tab
+window.addEventListener('blur', function() {
+    focusOut = true;
+    document.title = "Breakup";
+  })
+  
+  window.addEventListener('focus', function() {
+    focusOut = false;
+    document.title = "Patch Up";
+  });
+
+// document.addEventListener("visibilitychange", function(){
+
+// })
+
+
+// var countDownDate = new Date().setSeconds(new Date().getSeconds() + 20);
+
+// function startDown() {
+//   // counter.innerText = counterNumber;
+//   var now = new Date().getTime();
+//   var distance = countDownDate - now;
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   counter.innerHTML = seconds;
+// }
