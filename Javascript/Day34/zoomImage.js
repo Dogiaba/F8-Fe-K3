1002457c969f456ede9c076ab7c142c8c0f11f32
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-10-16 20:59:39 +0700

    day34

--- Javascript/Day34/zoomImage.js
+++ Javascript/Day34/zoomImage.js
@@ -1,12 +1,18 @@
 var lens = document.querySelector("#lens");
 var image1 = document.querySelector("#img1");
-var image2 = document.querySelector("#img2");
+var image2 = document.getElementById("#img2");
 
 image1.addEventListener("mousemove", function (e) {
+  var pos;
+
+
   var x = e.clientX - e.target.offsetLeft;
   var y = e.clientY - e.target.offsetTop;
   //   var x = lens.offsetWidth / e.offsetWidth;
   //   var y= lens.offsetHeight / e.offsetHeight;
+
+
+
 
   lens.style.display = "block";
 
@@ -16,7 +22,8 @@ image1.addEventListener("mousemove", function (e) {
   lens.style.left = x + "px";
   lens.style.top = y + 250 + "px";
 
-  image2.style.backgroundPosition = ((x - (500 /2/6)) * -6 + 'px') + ((y - (500 /2/6)) * -6 + 'px');
+  image2.style.backgroundPosition =
+    (x - (500 / 2 / 6)) * -6 + "px" + (y - (500 / 2 / 6) + 250) * -6 + "px";
 });
 
 image1.addEventListener("mouseout", function () {
