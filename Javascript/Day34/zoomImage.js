cc19ff0bde8f0ac182a7cb9f7b038a4788bb1674
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-10-18 00:22:21 +0700

    fix F12 btnDay33

--- Javascript/Day34/zoomImage.js
+++ Javascript/Day34/zoomImage.js
@@ -14,19 +14,36 @@ image1.addEventListener("mousemove", function (e) {
   var y = e.clientY - e.target.offsetTop;
 
   lens.style.display = "block";
-  //   lens.style.left = x + 76 + "px";
-  //   lens.style.top = y + 299  + "px";
-
+  
   lens.style.left = x + "px";
   lens.style.top = y + 250 + "px";
 
-  image2.style.backgroundSize = `${image2.clientWidth*cx}px ${image2.clientHeight*cy}px`;
+
+
+  if (x >= image1.width - lens.offsetWidth / 2) {x = image1.width - lens.offsetWidth;}
+  if (x < lens.offsetWidth / 2) {x = 0;}
+  if (y >= image1.height - lens.offsetHeight / 2) {y = image1.height - lens.offsetHeight;}
+  if (y < lens.offsetHeight / 2) {y = 0;}
+
+
+
+  
+  image2.style.backgroundSize = `${image2.clientWidth * cx}px ${
+    image2.clientHeight * cy
+  }px`;
 
   /* Display what the lens "sees": */
   image2.style.backgroundPosition =
-    "-" + (x - (500 / 2 / 6)) * cx + "px " + "-" + (y - (500 / 2 / 6) + 250) * cy  + "px";
+    "-" +
+    (x - 500 / 2 / 6)*  cx +
+    "px " +
+    "-" +
+    (y - 500 / 2 / 6 + 250)* cy +
+    "px";
 });
 
+// lens.style.left = x + "px";
+// lens.style.top = y + 500+ "px";
 
 image1.addEventListener("mouseout", function () {
   lens.style.display = "none";
