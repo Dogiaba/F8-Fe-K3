76877080572d5697fce2e3b4fde0e45d145afa5f
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-10-20 23:17:36 +0700

    day35

---
+++ Javascript/Day35/searchVoice.js
@@ -0,0 +1,36 @@
+var btnVoice = document.querySelector(".btn");
+var actice = document.querySelector(".actice");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = "vi-VN";
+
+btnVoice.addEventListener("click", function () {
+  recognition.start();
+  actice.innerHTML = "Hãy nói nội dung bạn muốn...";
+
+  recognition.onresult = function (event) {
+    const voiceResult = event.results[0][0].transcript.toLowerCase();
+    console.log(voiceResult);
+    voiceResult.innerHTML = `<span>Đã hoàn thành</span>
+    <span>Đang tìm kiếm ${voiceResult}</span>`;
+    handleVoice(voiceResult);
+  };
+});
+
+
+
+function handleVoice(listen) {
+  for (let item of keywords) {
+    console.log(listen.toLowerCase(), item.keyword);
+    if (listen.toLowerCase().includes(item.keyword)) {
+      console.log(item.url);
+      window.open(item.url, "_blank");
+      break;
+    }
+  }
+  actice.innerHTML = "Không tìm được nội dung yêu cầu";
+}
+
+
