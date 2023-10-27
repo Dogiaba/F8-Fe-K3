ca45ef33cb814350287859667b70300ddc5980d7
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-10-28 01:04:01 +0700

    up day37

--- Javascript/Day37/skeletonLoading.js
+++ Javascript/Day37/skeletonLoading.js
@@ -1,51 +1,50 @@
-// var dataProduct= [
-//     {
-//         id:1,
-//         name:"WD 2TB Elements",
-//         type:"electronics",
-//         vote:3.4,
-//         img: "./img/product.jpg",
-//     },
-//     {
-//         id:2,
-//         name:"WD 2TB Elements",
-//         type:"electronics",
-//         vote:3.4,
-//         img: "./img/product.jpg",
-//     },
-//     {
-//         id:3,
-//         name:"WD 2TB Elements",
-//         type:"electronics",
-//         vote:3.4,
-//         img: "./img/product.jpg",
-//     },
-// ]
-
-// const banner = document.querySelector(".banner");
-// const itemImg = document.querySelector(".item-img");
-// const itemTitle = document.querySelector(".item-title");
-// const price = document.querySelector(".price");
-// const itemBtn = document.querySelector(".item-btn");
-// const itemStart = document.querySelector(".item-start");
-// const nameType = document.querySelector(".nameType")
-// const voteStart = document.querySelector(".voteStart");
-
-// const render = ()=>{
-//     banner.classList.remove("skeleton");
-//     itemImg.classList.remove("skeleton");
-//     itemTitle.classList.remove("skeleton");
-//     price.classList.remove("skeleton");
-//     itemBtn.classList.remove("skeleton");
-//     itemStart.classList.remove("skeleton")
-//     nameType.classList.remove("skeleton")
-//     voteStart.classList.remove("skeleton")
-// }
 const wrapper = document.querySelector(".wrapper");
+const banner = document.querySelector(".banner");
+const productContent = document.querySelector(".products");
 setTimeout(()=> {
     wrapper.classList.remove("skeleton")
-},1000)
+},3000)
 
-// setTimeout(() =>{
-//     render();
-// },1000)
+
+function render(dataProduct) {
+  Promise.all(
+    dataProduct.map((value, index) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(()=>{
+                resolve(value);
+                productContent.classList.remove("skeleton")
+            },3000)
+          });
+
+    })
+  ).then((data)=>{
+    const result = data.reduce((prev , value)=> {
+        return prev  + 
+        `
+        <div class="item-product">
+        <div class="item-img ">
+          <img src="${value.img}" alt="product" />
+        </div>
+        <h3 class="item-title ">${value.name}</h3>
+        <div class="item-info">
+          <span class="nameType">${value.type}</span>
+          <div class="item-vote">
+            <span class="voteStart ">${value.vote}</span>
+            <span class="item-start ">
+              <i class="fa-solid fa-star"></i>
+            </span>
+          </div>
+        </div>
+        <h3 class="price ">${value.price}</h3>
+        <div class="item-btn ">
+          <button class="item-addBtn">Add to card</button>
+          <button class="item-buyBtn">Buy now</button>
+        </div>
+      </div>
+        `
+    },``)
+    productContent.innerHTML= result
+    console.log(result);
+  })
+}
+render(dataProduct);
