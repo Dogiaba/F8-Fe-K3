f63cbcd7ed6f699ddc96918370988db9f6eae29c
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-11-07 00:12:44 +0700

    up

---
+++ Javascript/Day40/quizzApp.js
@@ -0,0 +1,74 @@
+import { client } from "./client.js";
+import { config } from "./config.js";
+const { PAGE_LIMIT } = config;
+
+const startQuizz = document.querySelector(".start-quizz");
+const startBtn = document.querySelector(".start-btn");
+const quizContent = document.querySelector(".quiz-content");
+const container = document.querySelector(".container");
+
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+const app = {
+    query: {},
+    render: function(question, options){
+        const innHtml = `
+        <p class="title-question">${question}</p>
+        <div class="questions">
+        ${options.map((option)=>`<button class="anw-btn">${option}</button>`).join("")}</div>
+        `
+        quizContent.innerHTML += innHtml;
+    },
+
+    addEvent: function(){
+
+        startBtn.addEventListener("click", ()=>{
+            startQuizz.classList.add("hide");
+            container.classList.remove("hide");
+            console.log("ok")
+
+        })
+
+        window.onload = ()=>{
+            startQuizz.classList.remove("hide");
+            container.classList.add("hide")
+        }
+    },
+
+    checker: function(optio){
+
+    },
+
+    getQuiz: async function(query = {}){
+        let queryString = new URLSearchParams(query).toString();
+        if (queryString) {
+          queryString = "?" + queryString;
+        }
+        const {data } = await client.get("quizz" + queryString);
+        console.log(data);
+        data.forEach((value, index) =>{
+            this.render(data[index].question, data[index].options)
+        })
+        
+        
+    },
+
+
+    start: function(){
+        Object.assign(this.query,{
+            _limit: PAGE_LIMIT,
+            _page: 1,
+        });
+        this.getQuiz(this.query);
+        this.addEvent();
+    }
+
+    
+}
+
+app.start();
+
+
