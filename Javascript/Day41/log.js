a11b295e831d279c89e2c515f35b8466c7dafbc6
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-11-10 16:45:47 +0700

    up

--- Javascript/Day41/log.js
+++ Javascript/Day41/log.js
@@ -1,4 +1,4 @@
-import {client} from "./client.js";
+import { client } from "./client.js";
 import { config } from "./config.js";
 
 client.setUrl(config.SERVER_AUTH_API);
@@ -6,18 +6,36 @@ client.setUrl(config.SERVER_AUTH_API);
 const root = document.querySelector("#root");
 
 const app = {
-    loginStatus: false,
-    render:function(){
-        root.innerHTML = this.isLogin() ?this.dashboasrd() : this.loginForm();
-    },
-    isLogin: function(){
-        return this.loginStatus;
-    },
+  loginStatus: false,
+  render: function () {
+    root.innerHTML = this.isLogin() ? this.homePage() : this.loginForm();
+  },
+  isLogin: function () {
+    return this.loginStatus;
+  },
 
+  homePage: function () {
+    return `<div class="container">
+        <nav class="nav-bar">
+        <div class="logo">
+          <a href="#"><img src="./img/Vector.jpg" alt="imgLogo" /></a>
+        </div>
+        <div class="infor-navRight">
+          <ul class="right-nav">
+            <li><a href="#">Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">English</a></li>
+            <li><a href="#" class="">Logout</a></li>
+            <li><a href="#"><i class="fa-solid fa-house"></i></a></li>
+          </ul>
+        </div>
+        <!-- .infor-navRight -->
+      </nav>
+        </div>`;
+  },
 
-
-    loginForm: function(){
-        return`
+  loginForm: function () {
+    return `
         <div class="container">
         <nav class="nav-bar">
           <div class="logo">
@@ -90,26 +108,78 @@ const app = {
         <!-- .content -->
       </div>
         
-        `
-    },
-    addEvent: function(){
-        root.addEventListener("submit", (e)=>{
-            e.preventDefault();
-            const form = [...new FormData(e.target)];
-            const data = Object.fromEntries(form);
-            this.login(data, e.target);
-            console.log(data);
-        });
+        `;
+  },
+  addEvent: function () {
+    root.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const form = [...new FormData(e.target)];
+      const data = Object.fromEntries(form);
+      this.login(data, e.target);
+      console.log(data);
+    });
+
+    root.addEventListener("click", (e) => {
+      if (e.target.classList.contains("logout")) {
+        e.preventDefault();
+        this.handLogout();
+      }
+    });
+  },
+  handLogout: function () {
+    //here
+    localStorage.removeItem("");
+    this.loginStatus = false;
+    this.render();
+  },
+  login: async function ({ email, password }, el) {
+    const { data: tokens, response } = await client.post("/auth/login", {
+      email,
+      password,
+    });
+    // if (!response.ok) {
+    //   this.showMess(el, "Login false");
+    //   return;
+    // }
+    localStorage.setItem("login_token", JSON.stringify(tokens));
+    this.loginStatus = true;
+    this.checkAuth();
+    this.render();
+  },
 
 
-        //hear
-    },
+  checkAuth: async function () {
+    if (localStorage.getItem("login-token")) {
+      try {
+        const { accessToken_token: accessToken } = JSON.parse(
+          localStorage.getItem("login-token")
+        );
+        if (!accessToken) {
+          throw new Error("Access TOken Not Exists");
+        }
+        root.innerHTML = `<div class="container">
+            <h2>...</h2></div>`;
+        this.loginStatus = true;
+        client.setToken(accessToken);
+        const { data: user, response } = await client.get("/users/profile");
 
-    start: function(){
-        this.render();
-        this.addEvent();
+        if (!response.ok) {
+          this.loginStatus = false;
+          return;
+        }
+        this.loginStatus = true;
+        this.user = user;
+      } catch (e) {
+        this.loginStatus = false;
+      }
+      this.render();
     }
+  },
 
-    
-}
+  start: function () {
+    this.render();
+    this.addEvent();
+    this.checkAuth();
+  },
+};
 app.start();
