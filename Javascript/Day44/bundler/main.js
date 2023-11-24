31559673614ccd9936c9a05fb17364427ceca4a4
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-11-25 02:19:31 +0700

    up

--- Javascript/Day44/bundler/main.js
+++ Javascript/Day44/bundler/main.js
@@ -1,42 +1,32 @@
-import './style.css'
-import {renderPage , routerNa} from "./src/Utils/Router.js"
-import { App } from './src/App.js';
-import {Error} from './src/Error.js'
-import { ProductDetail } from './src/Pages/ProductDetail.js';
+import "./style.css";
+import { renderPage, routerNa } from "./src/Utils/Router.js";
+import { App } from "./src/App.js";
+import { Error } from "./src/Pages/Error.js";
+import { ProductDetail } from "./src/Pages/ProductDetail.js";
 
-const {root:roots, DefaultLayout} = App();
-
+const { root: roots, DefaultLayout } = App();
 const app = document.querySelector("#app");
 const btnBack = document.querySelector(".btnBack");
 
-function render(component, DefaultLayout, params = null){
-    let stringHtml = DefaultLayout;
-    const pattern = /{body}/;
+function render(component, DefaultLayout, params = null) {
+  let stringHtml = DefaultLayout;
+  const pattern = /{body}/;
 
-    if(params){
-        app.innerHTML = stringHtml.replace(pattern,component(params));
-    }else{
-        
-        console.log(app.innerHTML = stringHtml.replace(pattern,component()));
-    }
-    
+  if (params) {
+    app.innerHTML = stringHtml.replace(pattern, component(params));
+  } else {
+    console.log((app.innerHTML = stringHtml.replace(pattern, component())));
+  }
 }
-roots.forEach(({path, component}, _)=> {
-    routerNa.on(path,(params)=>{
-        render(component,DefaultLayout, params )
-    })
-}); 
+roots.forEach(({ path, component }, _) => {
+  routerNa.on(path, (params) => {
+    render(component, DefaultLayout, params);
+  });
+});
+
 routerNa.resolve();
-routerNa.notFound(()=> {
+routerNa
+  .notFound(() => {
     app.innerHTML = Error();
-}).resolve();
-
-
-// btnBack.onclick = function(){
-//     console.log("ok")
-// }
-
-
-function navigate(){
-    console.log("ok")
-}
+  })
+  .resolve();
