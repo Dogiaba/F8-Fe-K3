9dd17cee6974af9e74baa589776c7bfba8dbc2d3
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-11-24 21:28:55 +0700

    day44

--- Javascript/Day44/bundler/main.js
+++ Javascript/Day44/bundler/main.js
@@ -1,20 +1,21 @@
 import './style.css'
 import { Home } from './src/Pages/Home.js';
-
 import {renderPage , routerNa} from "./src/Utils/Router.js"
 import { App } from './src/App.js';
+import {Error} from './src/Error.js'
 
 const {root:roots, DefaultLayout} = App();
 
-
+const app = document.querySelector("#app");
 function render(component, DefaultLayout, params = null){
     let stringHtml = DefaultLayout;
     const pattern = /{body}/;
-    const app = document.querySelector("#app");
+
     if(params){
         app.innerHTML = stringHtml.replace(pattern,component(params));
     }else{
-        app.innerHTML = stringHtml.replace(pattern,component());
+        
+        console.log(app.innerHTML = stringHtml.replace(pattern,component()));
     }
     
 }
@@ -22,6 +23,13 @@ roots.forEach(({path, component}, _)=> {
     routerNa.on(path,(params)=>{
         render(component,DefaultLayout, params )
     })
-});
+}); 
 routerNa.resolve();
+routerNa.notFound(()=> {
+    app.innerHTML = Error();
+}).resolve();
+
+
+
+
 
