35c062b7f2e20b3a563632e50d4d1996745d0da8
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-11-29 23:29:47 +0700

    day45

---
+++ Javascript/Day45/todolist/src/Components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewToDoFrom from './NewToDoFrom'
+function Todo () {
+
+  return (
+    <div>
+        <NewToDoFrom/>
+    </div>
+  )
+}
+
+export default Todo
---
+++ Javascript/Day45/todolist/src/Components/Todolist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Todolist() {
+  return (
+    <div>Todolist thay ddooir cho todo</div>
+  )
+}
+
+export default Todolist
