cf68871f1ea61b1e5163e570f18f17f2bce33d5c
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2023-12-25 04:55:16 +0700

    day51

---
+++ Javascript/Day51/trello/src/store/Provider.jsx
@@ -0,0 +1,15 @@
+import { createContext, useReducer } from "react";
+import { initialState, reducer } from "../reducers/rootReducer";
+
+export const GlobaContext = createContext();
+
+function Provider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <GlobaContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobaContext.Provider>
+  );
+}
+
+export default Provider;
