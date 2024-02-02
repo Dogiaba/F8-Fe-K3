26d1e55daaf2d31c28495fe1532f3b6e69275cd7
Bao Minh <85350720+Dogiaba@users.noreply.github.com>
2024-02-02 09:47:23 +0700

    fix share

--- NextJS/Day53/mindmap/src/app/(client)/login/page.js
+++ NextJS/Day53/mindmap/src/app/(client)/login/page.js
@@ -31,7 +31,7 @@ const pageLogin = () => {
                   />
                 </svg>
               </div>
-              <span className="ml-4">Sign Up with Google(Bảo Trì)</span>
+              <span className="ml-4">Sign Up with Google</span>
             </button>
 
             <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
